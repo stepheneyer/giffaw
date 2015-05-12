d0b75483d5e73828a0e4b09c10de96ff2c06afd7
“Stephen <stephen@stepheneyer.comssh-keygen -t rsa -C stephen@stepheneyer.com>
2015-05-12 08:42:53 -0700

    Basic search

--- base.js
+++ base.js
@@ -1,4 +1,40 @@
-$(document).ready(function(){
-  // code in here!
-  console.log("here's your data:", mockData)
-})
+$(document).on("ready", function(){
+
+//Set global variables
+
+var searchRequest;
+var newSearch;
+var apiData;
+
+//Setup event listener for search field
+
+$("#search").on("change", function(event) {
+  searchRequest = $(this).val();
+  // event.preventDefault();
+  appendSearch(searchRequest);
+});
+
+})//end of document ready
+
+
+function removeSpaces(string) {
+ return string.split(' ').join('');
+}
+
+//Add new search term to API call
+
+function appendSearch (searchRequest) {
+	newSearch = ("http://api.giphy.com/v1/gifs/search?q=" + searchRequest + "&limit=10&api_key=dc6zaTOxFJmzC");
+	console.log(newSearch);
+	$.get(newSearch, render);
+}
+
+//Render API results into body of HTML
+
+function render(apiData){
+  var newData = apiData.data;
+  newData.forEach(function(item,i){
+    var thumb = item.images.fixed_width.url;
+    $("body").append("<img src='" + thumb + "'>"); // mind the single vs. double quotes!
+  })  
+}
